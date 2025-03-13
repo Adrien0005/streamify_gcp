5987464c7d275dd0c1c4a145d8dfad34de2ec6c9
adrien.lallemand <adrien.lallemand@datatonic.com>
2025-03-13 11:25:40 -0400

    - add cloud scheduler hourly - update diagram

--- _init/_deploy.sh
+++ _init/_deploy.sh
@@ -1,8 +1,25 @@
 # setup pubsub
+
 # setup schema pubsub
+
 # setup cloud functions
+
 # setup dataflow
-# build docker image
-# push docker image
+
+# build & push docker image
+gcloud builds submit --tag gcr.io/dt-dta-adrien-sandbox-dev/dbt-streamify:latest
+
 # create cloud run based on latest docker image
+gcloud run jobs create dbt-streamify \
+  --image gcr.io/dt-dta-adrien-sandbox-dev/dbt-streamify:latest \
+  --region us-central1 
+
+# schedule the dbt run
+gcloud scheduler jobs create http dbt-scheduler-job \
+  --schedule "0 * * * *" \
+  --uri "https://us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/dt-dta-adrien-sandbox-dev/jobs/dbt-streamify:run" \
+  --http-method POST \
+  --oauth-service-account-email cloud-scheduler@dt-dta-adrien-sandbox-dev.iam.gserviceaccount.com \
+  --location us-central1
+
 # launch event generator
