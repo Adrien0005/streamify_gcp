2b9085a50b98e279fc33940d234208fbcfd11e71
adrien.lallemand <adrien.lallemand@datatonic.com>
2025-03-13 15:25:08 -0400

    - add env variables to sh scripts - update comments in cloudbuild.yml

--- _init/create_dbt_cloud_run.sh
+++ _init/create_dbt_cloud_run.sh
@@ -1,15 +1,17 @@
+source .env
+
 # start colima
 
 # Build and push the container to Google Container Registry
-gcloud builds submit --tag gcr.io/dt-dta-adrien-sandbox-dev/dbt-streamify:latest
+gcloud builds submit --tag gcr.io/${PROJECT_ID}/${CLOUD_RUN_NAME}:latest
 
 # Deploy a Cloud Run job
-gcloud run jobs create dbt-streamify \
-  --image gcr.io/dt-dta-adrien-sandbox-dev/dbt-streamify:latest \
-  --region us-central1 
+gcloud run jobs create ${CLOUD_RUN_NAME} \
+  --image gcr.io/${PROJECT_ID}/${CLOUD_RUN_NAME}:latest \
+  --region ${REGION}
 
 # Execute manually if needed
-# gcloud run jobs execute dbt-streamify --region us-central1
+# gcloud run jobs execute ${CLOUD_RUN_NAME} --region ${REGION}
 
 # Docker Push to cloud REgistry
 # Cloud Run utilise la derniere v de mon Container
